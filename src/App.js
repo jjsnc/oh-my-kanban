1c8857874cd12eb3e03af90b495161e5bffbad0b
早起喝燕麦 <zhuowangyi@163.com>
2024-11-13 18:13:41 +0800

    嵌套选择器

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import { css } from "@emotion/react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const KanbanBoardStyle = css`
-  flex: 10;
-  display: flex;
-  flex-direction: row;
-  gap: lrem;
-  margin: 0 lrem lrem;
-`;
-
 const ongoingList = [
   { title: "开发任务-4", status: "22-05-22 18:15" },
   { title: "开发任务-6", status: "22-05-22 18:15" },
@@ -59,14 +51,53 @@ const KanbanNewCard = ({ onSubmit }) => {
 };
 
 const KanbanBoard = ({ children }) => (
-  <main css={KanbanBoardStyle}>{children}</main>
+  <main
+    css={css`
+      flex: 10;
+      display: flex;
+      flex-direction: row;
+      gap: 1rem;
+      margin: 0 1rem 1rem;
+    `}
+  >
+    {children}
+  </main>
 );
 
 const KanbanColumn = ({ title, className, children }) => {
-  const combinedClassName = `kanban-column ${className}`;
-
   return (
-    <section className={combinedClassName}>
+    <section
+      className={className}
+      css={css`
+        flex: 1 1;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid gray;
+        border-radius: 1rem;
+        & > ul {
+          flex: 1;
+          flex-basis: 0;
+          margin: 1rem;
+          padding: 0;
+          overflow: auto;
+        }
+        & > h2 {
+          margin: 0.6rem 1rem;
+          padding-bottom: 0.6rem;
+          border-bottom: 1px solid gray;
+          & > button {
+            float: right;
+            margin-top: 0.2rem;
+            padding: 0.2rem 0.5rem;
+            border: 0;
+            border-radius: 1rem;
+            height: 1.8rem;
+            line-height: 1rem;
+            font-size: 1rem;
+          }
+        }
+      `}
+    >
       <h2>{title}</h2>
       <ul>{children}</ul>
     </section>
