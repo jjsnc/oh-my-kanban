4f692a24710d7ed1bdaa939fba5ac6ab2cc8bf37
早起喝燕麦 <zhuowangyi@163.com>
2024-11-18 17:08:53 +0800

    抽离KanbanColumn

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { css } from "@emotion/react";
 import logo from "./logo.svg";
 import "./App.css";
 import { KanbanBoard } from "./KanbanBoard";
+import { KanbanColumn } from "./KanbanColumn";
 
 const kanbanCardStyles = css`
   margin-bottom: 1rem;
@@ -72,75 +73,6 @@ const KanbanNewCard = ({ onSubmit }) => {
         />
       </div>
     </li>
-  );
-};
-
-const KanbanColumn = ({
-  children,
-  bgColor,
-  title,
-  setIsDragSource = () => {},
-  setIsDragTarget = () => {},
-  onDrop
-}) => {
-  return (
-    <section
-      onDragStart={() => {
-        setIsDragSource(true);
-      }}
-      onDragOver={(evt) => {
-        evt.preventDefault();
-        evt.dataTransfer.dropEffect = "move";
-        setIsDragTarget(true);
-      }}
-      onDragLeave={(evt) => {
-        evt.preventDefault();
-        evt.dataTransfer.dropEffect = "none";
-        setIsDragTarget(false);
-      }}
-      onDrop={(evt) => {
-        evt.preventDefault();
-        onDrop && onDrop(evt);
-      }}
-      onDragEnd={(evt) => {
-        evt.preventDefault();
-        setIsDragSource(false);
-        setIsDragTarget(false);
-      }}
-      css={css`
-        flex: 1 1;
-        display: flex;
-        flex-direction: column;
-        border: 1px solid gray;
-        border-radius: 1rem;
-        background: ${bgColor};
-        & > ul {
-          flex: 1;
-          flex-basis: 0;
-          margin: 1rem;
-          padding: 0;
-          overflow: auto;
-        }
-        & > h2 {
-          margin: 0.6rem 1rem;
-          padding-bottom: 0.6rem;
-          border-bottom: 1px solid gray;
-          & > button {
-            float: right;
-            margin-top: 0.2rem;
-            padding: 0.2rem 0.5rem;
-            border: 0;
-            border-radius: 1rem;
-            height: 1.8rem;
-            line-height: 1rem;
-            font-size: 1rem;
-          }
-        }
-      `}
-    >
-      <h2>{title}</h2>
-      <ul>{children}</ul>
-    </section>
   );
 };
 
---
+++ src/KanbanColumn.js
@@ -0,0 +1,73 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React from "react";
+
+export function KanbanColumn({
+  children,
+  bgColor,
+  title,
+  setIsDragSource = () => {},
+  setIsDragTarget = () => {},
+  onDrop,
+}) {
+  const KanbanColumnStyles = css`
+    flex: 1 1;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid gray;
+    border-radius: 1rem;
+    background: ${bgColor};
+    & > ul {
+      flex: 1;
+      flex-basis: 0;
+      margin: 1rem;
+      padding: 0;
+      overflow: auto;
+    }
+    & > h2 {
+      margin: 0.6rem 1rem;
+      padding-bottom: 0.6rem;
+      border-bottom: 1px solid gray;
+      & > button {
+        float: right;
+        margin-top: 0.2rem;
+        padding: 0.2rem 0.5rem;
+        border: 0;
+        border-radius: 1rem;
+        height: 1.8rem;
+        line-height: 1rem;
+        font-size: 1rem;
+      }
+    }
+  `;
+  return (
+    <section
+      onDragStart={() => {
+        setIsDragSource(true);
+      }}
+      onDragOver={(evt) => {
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = "move";
+        setIsDragTarget(true);
+      }}
+      onDragLeave={(evt) => {
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = "none";
+        setIsDragTarget(false);
+      }}
+      onDrop={(evt) => {
+        evt.preventDefault();
+        onDrop && onDrop(evt);
+      }}
+      onDragEnd={(evt) => {
+        evt.preventDefault();
+        setIsDragSource(false);
+        setIsDragTarget(false);
+      }}
+      css={KanbanColumnStyles}
+    >
+      <h2>{title}</h2>
+      <ul>{children}</ul>
+    </section>
+  );
+}
